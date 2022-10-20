d6783b9e13c22371409cfcbe24147e90af23d50a
marifat0 <marifatalhanif@gmail.com>
2022-10-20 20:00:21 +0700

    new

--- script.js
+++ script.js
@@ -1,34 +1,22 @@
-const forms = document.getElementById('form');
-const nemInput = document.querySelector('#neym');
-const button = document.querySelector('#submitt');
-
- let users = []
- const addUser = (ev) => {
-    console.log('hey')
-    let user = {
-        name : document.getElementById('neym').value,
-        age : document.getElementById('ageU').value,
-        phoneNumber : document.getElementById('telnum').value,
-        email : document.getElementById('emailInput').value,
-        passw : document.getElementById('password').value,
-        coursesA : document.querySelector('#courseA').value,
-        coursesB : document.querySelector('#courseB').value
-
-     }
-    ev.preventDefault();
-    
-users.push(addUser);
-document.forms[0].reset();
-console.log('added', {users});
+let submitBtn = document.getElementById('submitt');
+let num = 0;
+submitBtn.addEventListener('click', (ev) => {
+    ev.preventDefault()
+    let clients = localStorage.getItem('clients')
+    clients = JSON.parse(clients)
+    if (clients == null){
+        clients = [];
     }
-
-
-
-// dont change this
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('submitt').addEventListener('click', addUser);
+    let data = JSON.stringify({
+        Name : document.getElementById('neym').value,
+        Age : document.getElementById('ageU').value,
+        Phone : document.getElementById('telnum').value,
+        Email : document.getElementById('emailInput').value,
+        Passw : document.getElementById('password').value,
+        Course : document.querySelector('input[name=Course]:checked').value
+    });
+    clients.push(data)
+    let dataStorage = localStorage.setItem('clients', JSON.stringify(clients));
+    console.log(dataStorage);
 });
-forms.addEventListener('submit', (e) => {
-   e.preventDefault();
-});
-
+console.log(localStorage.getItem('clients'))
